4f4523f9fd0475231e1318f50eb791b25daf1bc2
Dongik Shin <sdimivy014@gmail.com>
2024-07-25 23:41:32 +0900

    edit main.py & photoarrange.py
    add dst dir function.

--- main.py
+++ main.py
@@ -4,25 +4,29 @@ from tkinter import filedialog
 from utils import photoarrange as pa
 
 
-def browse_directory():
+def browse_directory(directory_entry):
     directory_path = filedialog.askdirectory()
     if directory_path:
         directory_entry.delete(0, tk.END)  # 기존의 내용 지우기
         directory_entry.insert(0, directory_path)
 
-def process_directory():
+def process_directory(directory_entry, dst_entry):
     selected_directory = directory_entry.get()
-    if selected_directory:
+    dst_directory = dst_entry.get()
+    if selected_directory and dst_entry:
         print(f"Selected Directory: {selected_directory}")
+        print(f"Destination Directory: {dst_directory}")
         # 이 곳에서 선택된 디렉토리를 처리하는 추가적인 로직을 작성할 수 있습니다.
-        pa.photoarrange(selected_directory)
-
+        pa.photoarrange(selected_directory, dst_directory)
     else:
         print("No directory selected.")
 
 def center_window(window, width, height):
-    # 창의 크기 설정
-    window.geometry(f"{width}x{height}")
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    x = (screen_width // 2) - (width // 2)
+    y = (screen_height // 2) - (height // 2)
+    window.geometry(f'{width}x{height}+{x}+{y}')
 
 
 
@@ -32,21 +36,27 @@ if __name__ == "__main__":
     root.title("나의 사진 정리")
 
     # 창의 가로 및 세로 크기 설정
-    window_width = 400
+    window_width = 500
     window_height = 150
     center_window(root, window_width, window_height)
 
     # 디렉토리 입력을 위한 Entry 위젯 생성
     directory_entry = tk.Entry(root, width=50)
-    directory_entry.pack(pady=10)
-
+    directory_entry.grid(row=0, column=1, padx=10, pady=10)
     # 디렉토리 선택 버튼 생성
-    select_button = tk.Button(root, text="찾아보기", command=browse_directory)
-    select_button.pack(pady=5)
+    select_button = tk.Button(root, text="찾아보기", command=lambda: browse_directory(directory_entry))
+    select_button.grid(row=0, column=0, padx=10, pady=10)
+
+    # 디렉토리 입력을 위한 Entry 위젯 생성
+    dst_entry = tk.Entry(root, width=50)
+    dst_entry.grid(row=1, column=1, padx=10, pady=10)
+    # 저장할 데렉토리 선택 버튼 생성
+    dst_button = tk.Button(root, text="저장위치", command=lambda: browse_directory(dst_entry))
+    dst_button.grid(row=1, column=0, padx=10, pady=10)
 
     # 디렉토리 처리 버튼 생성
-    process_button = tk.Button(root, text="사진 정리하기", command=process_directory)
-    process_button.pack(pady=10)
+    process_button = tk.Button(root, text="사진 정리하기", command=lambda: process_directory(directory_entry, dst_entry))
+    process_button.grid(row=2, column=0, columnspan=2, padx=10, pady=10)
 
     # GUI 창 실행
     root.mainloop()
--- utils/photoarrange.py
+++ utils/photoarrange.py
@@ -2,6 +2,7 @@ import os
 from PIL import Image
 from datetime import datetime
 import shutil
+import logging
 
 # 이미지 파일 확장자
 IMAGE_EXTENSIONS = ['.jpg', '.jpeg', '.png']
@@ -20,33 +21,49 @@ def get_image_date_taken(image_path):
         print(f"Error reading image metadata: {e}")
     return None
 
-def organize_photos(source_dir):
+def organize_photos(source_dir, dst):
     """ 사진 정리 함수 """
     if not os.path.exists(source_dir):
-        print("Path not exists...")
-        
+        print("ERROR: Path not exists:", source_dir)
+    if not os.path.exists(dst):
+        dst = source_dir
+        print("ERROR: Path not exists:", dst)
+        print(">>> Process at default directory:", source_dir)
+
+    idx = 0
     for filename in os.listdir(source_dir):
         if any(filename.lower().endswith(ext) for ext in IMAGE_EXTENSIONS):
             file_path = os.path.join(source_dir, filename)
+            _, ext = os.path.splitext(file_path)
             date_taken = get_image_date_taken(file_path)
             if date_taken:
                 year = date_taken.year
-                month = date_taken.strftime('%m-%B')
-                target_dir = os.path.join(source_dir, str(year), month)
+                month = date_taken.strftime('%B')
+                target_dir = os.path.join(dst, str(year), month)
                 os.makedirs(target_dir, exist_ok=True)
+                # 새로운 파일 이름 지정
+                new_filename = date_taken.strftime("%Y-%m-%d") + f"_{idx:04}" + ext
+                # 로깅 설정
+                logging.basicConfig(
+                    filename=os.path.join(target_dir, 'file_log.log'), 
+                    level=logging.INFO, format='%(asctime)s - %(message)s'
+                )
                 try:
-                    shutil.move(file_path, os.path.join(target_dir, filename))
-                    print(f"Moved {filename} to {os.path.join(target_dir, filename)}")
+                    shutil.move(file_path, os.path.join(target_dir, new_filename))
+                    print(f"Moved {filename} to {os.path.join(target_dir, new_filename)}")
+                    logging.info(f"Moved {filename} to {os.path.join(target_dir, new_filename)}")
                 except Exception as e:
                     print(f"Failed to move {filename}: {e}")
+                    logging.error(f"Failed to move {filename}: {e}")
+        idx += 1
+
+    print("Photo organization completed.")
 
 def test_module():
     print("\"photoarragement.py\" module has compiled without errors and is operational.")
 
-def photoarrange(source_directory):
-    organize_photos(source_directory)
-    print("Photo organization completed.")
-
+def photoarrange(source_directory, dst):
+    organize_photos(source_directory, dst)
 
 if __name__ == "__main__":
     source_directory = input("Enter the path to the directory containing photos: ")
